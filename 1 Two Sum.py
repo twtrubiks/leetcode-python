6819226b35770938ff1ecf921d7695d5ef511464
twtrubiks <twtrubiks@users.noreply.github.com>
2016-03-31 10:19:06 +0800

    Update 1 Two Sum.py

--- 1 Two Sum.py
+++ 1 Two Sum.py
@@ -8,6 +8,8 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
+
+Run Time : 6196 ms
 '''
 class Solution(object):
     def twoSum(self, nums, target):
