b9264291ce803a1b3ce08f80c500079d60485fff
twtrubiks <twtrubiks@gmail.com>
2017-11-27 22:28:29 +0800

    add logic_tutorial (no leetcode)

---
+++ logic_tutorial.py
@@ -0,0 +1,28 @@
+def trunk_1(arr_1, size_1):
+    result_1 = []
+    while arr:
+        pop_data = [arr_1.pop(0) for _ in range(size_1)]
+        result_1.append(pop_data)
+    return result_1
+
+
+def trunk_2(arr_2, size_2):
+    arrs = []
+    while len(arr_2) > size_2:
+        pice = arr_2[:size_2]
+        arrs.append(pice)
+        arr_2 = arr_2[size:]
+    arrs.append(arr_2)
+    return arrs
+
+
+if __name__ == "__main__":
+    '''
+    arr = [1, 2, 3, 4, 5, 6]
+    size = 2
+    result = [[1, 2], [3, 4], [5, 6]]
+    '''
+    arr = [1, 2, 3, 4, 5, 6]
+    size = 2
+    result = trunk_2(arr, size)
+    print(result)
