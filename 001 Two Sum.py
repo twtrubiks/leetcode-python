8b65b76238b49517a7c2f29f88147076699fd264
twtrubiks <twtrubiks@gmail.com>
2017-11-29 22:35:50 +0800

    A better solution (use python3)

--- 001 Two Sum.py
+++ 001 Two Sum.py
@@ -1,16 +1,17 @@
 '''
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
-You may assume that each input would have exactly one solution.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 Example:
+
 Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
-
-Run Time : 6196 ms
 '''
+
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -18,20 +19,14 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        position=[];
-        run=True;
-        for i in range(len(nums)-1):
-            for count in list(range(i+1,len(nums),1)):
-                if(nums[i]+nums[count]==target and run):
-                    position.append(i);
-                    position.append(count);
-                    return position;
-                    run=False;
-                    break;
-            if(run == False):
-               break; 
-               
-if __name__=="__main__":
-    nums = [2, 7, 11, 15]
-    target = 9
-    print Solution().twoSum(nums, target)             
+        dict_temp = dict()
+        for index, value in enumerate(nums):
+            if target - value in dict_temp:
+                return [dict_temp[target - value], index]
+            dict_temp[value] = index
+
+
+if __name__ == "__main__":
+    nums = [3, 2, 4]
+    target = 6
+    print(Solution().twoSum(nums, target))
